4e17f99c6bc31c9666da5eda44a56dc13152edc2
Akachukwu Egboluche <77286113+cryptoduke01@users.noreply.github.com>
2024-12-08 11:11:05 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: "float 5s ease-in-out infinite",
+        bounce: "bounce 3s infinite",
+        pulse: "pulse 3s infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(-10px)" },
+          "50%": { transform: "translateY(10px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
